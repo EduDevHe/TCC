ad8a2fdf11fecba5b1a86c5acdfee9464059721d
EduDevHe <edu.he.dev@gmail.com>
2021-02-12 22:52:20 -0300

    mostrar o json.funcoes na tela

--- scripts/script.js
+++ scripts/script.js
@@ -12,15 +12,26 @@ fetch("json/lpi.json")
  })
  .then(function(json){
    var coMandos = json.comandos;
+   var fuCoes = json.funcoes;
    
-   var tabel = "<table>";
+   var objetosFuncoes =Object.keys(fuCoes);
+   var mapaDeObjetos = objetosFuncoes.map((objetosFuncoes)=>{ 
+     return objetosFuncoes + " " + fuCoes[objetosFuncoes];
+   });
+  console.log(mapaDeObjetos);
+  
+  var tabel = "<table>";
    tabel += "<tr><td>COMANDOS</td></tr>"
- 
+  
    coMandos.forEach(osComandos => {
-     console.log(osComandos);
-    tabel += "<li>" +`${osComandos} \n` + "<li>" ;
+     //console.log(osComandos);
+    tabel += "<li> " +`${osComandos} \n` + "<li>" ;
   });
   
+  mapaDeObjetos.forEach(asFuncoes =>{
+    //console.log(asFuncoes);
+    tabel += "<tr>" + "<li> " +`${asFuncoes} \n` + "<li>" + "</tr>"  ;
+  });
    
    tabel += "</table>"; 
    document.querySelector('#tabela').innerHTML = tabel;
