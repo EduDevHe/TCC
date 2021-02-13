e66600dd5ded8f6f1080aa88665bbd8c91013f27
EduDevHe <edu.he.dev@gmail.com>
2021-02-12 23:18:58 -0300

    codigo comentado

--- scripts/script.js
+++ scripts/script.js
@@ -6,29 +6,34 @@ btn.addEventListener('click',(e)=>{
 })
 */
 
+//pega o json/lpi.json
 fetch("json/lpi.json")
  .then(function(resp){
      return resp.json();
  })
  .then(function(json){
+   //pega o json.comando e atribui em uma variavel
    var coMandos = json.comandos;
+   //pega o json.funcoes e atribui em uma varivel
    var fuCoes = json.funcoes;
    
-   var objetosFuncoes =Object.keys(fuCoes);
-   var mapaDeObjetos = objetosFuncoes.map((objetosFuncoes)=>{ 
-     return objetosFuncoes + " " + fuCoes[objetosFuncoes];
+   //transforma o objeto json.funcoes em um array
+   var arrayFuncoes =Object.keys(fuCoes);
+   // mapeia o array
+   var mapaArrayFuncoes = arrayFuncoes.map((arrayFuncoes)=>{ 
+     return arrayFuncoes + " " + fuCoes[arrayFuncoes];
    });
-  console.log(mapaDeObjetos);
+  console.log(mapaArrayFuncoes);
   
   var tabel = "<table>";
-   tabel += "<tr><td>COMANDOS</td></tr>"
-  
+   tabel += "<tr><td>COMANDOS</td></tr>";
+   // percorre o array comandos do json.comandos
    coMandos.forEach(osComandos => {
      //console.log(osComandos);
     tabel += "<li> " +`${osComandos} \n` + "<li>" ;
   });
-  
-  mapaDeObjetos.forEach(asFuncoes =>{
+  //percorre o objeto funcoes que foi tansformado em array
+  mapaArrayFuncoes.forEach(asFuncoes =>{
     //console.log(asFuncoes);
     tabel += "<tr>" + "<li> " +`${asFuncoes} \n` + "<li>" + "</tr>"  ;
   });
